e379419fa0de8baeb89978e59adc46159667335f
Gaël Haméon <gael.hameon@gmail.com>
2023-04-17 22:32:06 +0200

    feat(public): create get props for entity class

---
+++ public/scripts/get-props-for-entity-class/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const execute = require('./src/getPropsForEntityClass');
+
+execute('C:/Users/8808412L/dev/bimo/public/core/entities/src');
---
+++ public/scripts/get-props-for-entity-class/src/getPropsForEntityClass.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-console */
+
+const { fsBimo, getAllFilesRecursively } = require('@bimo/core-utils-filesystem');
+const path = require('path');
+
+async function execute(rootDir) {
+  console.log(`Will execute get-props-for-entity-class in ${rootDir}`);
+  const allFiles = [];
+  await getAllFilesRecursively(rootDir, allFiles);
+
+  const entityFiles = allFiles.filter((file) => !file.name.match(/Collection\.ts$/));
+  const [firstFile] = entityFiles;
+  const myFiles = [firstFile];
+  await Promise.all(entityFiles.map(async (file) => handleFile(file.fullPath)));
+
+  console.log('done');
+}
+module.exports = execute;
+
+const propNameMatcher = /this\.(\w+) = /;
+const classKeyMatcher = /(\w+)\.ts/;
+const requireMatcher = /const (\{ .* \}) = require\((.*)\);/;
+const requireMatcher2 = /const (\w+) = require\((.*)\);/;
+const linesToDelete = [
+  '/* Linked Classes */',
+  '/* Serialization utilities dependencies */',
+  '/* Class definition */',
+
+];
+const classMatcher = /export class (\w+) extends Item \{/;
+const constructorPropsMatcher = /constructor\(props\)/;
+
+async function handleFile(fullPath) {
+  const contents = await fsBimo.readFile(fullPath, { encoding: 'utf8' });
+  const classKey = path.basename(fullPath).match(classKeyMatcher)[1];
+  console.log(classKey);
+  /** @type {string[]} */
+  const lines = contents.split('\n')
+    .map((line) => {
+      if (linesToDelete.includes(line)) {
+        return '';
+      }
+      return line;
+    })
+    .map((line) => line.replace(requireMatcher, 'import $1 from $2'))
+    .map((line) => line.replace(requireMatcher2, 'import $1 from $2'))
+    .map((line) => line.replace(/^class /, 'export class '))
+    .map((line) => line.replace('module.exports = ', 'export default '))
+    .map((line) => line.replace(classMatcher, 'export class $1 extends Item<$1> {'))
+    .map((line) => line.replace(constructorPropsMatcher, `constructor(props:${classKey}Props)`));
+  const startOfConstructor = lines.findIndex((line) => line.match(/constructor/));
+  if (startOfConstructor === -1) return;
+  const linesAfterConstructor = lines.slice(startOfConstructor + 1);
+  const endOfConstructor = linesAfterConstructor.findIndex((line) => line.match(/ {2}\}/));
+  const linesOfConstructor = linesAfterConstructor.slice(1, endOfConstructor);
+  const propNames = linesOfConstructor.map((line) => {
+    const match = line.match(propNameMatcher);
+    if (!match) return false;
+    return match[1];
+  }).filter(Boolean);
+  const propsDeclarationLines = propNames.map((propName) => `  ${propName}: string;`);
+  lines.splice(startOfConstructor, 0, ...propsDeclarationLines);
+
+  const interfaceDeclarationLines = [
+    `export interface ${classKey}Props {`,
+    ...propsDeclarationLines,
+    `}`,
+    '',
+  ];
+  lines.splice(
+    lines.findIndex((line) => line.match(/export class /)),
+    0,
+    ...interfaceDeclarationLines,
+  );
+
+  console.log(lines);
+}
