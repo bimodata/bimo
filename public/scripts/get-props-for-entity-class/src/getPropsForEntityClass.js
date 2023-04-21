8564c0220a4b6d1d6478a1098ff590ffff118e2d
Gaël Haméon <gael.hameon@gmail.com>
2023-04-21 23:28:46 +0200

    feat(public): improve getPropsForEntityClass script

--- public/scripts/get-props-for-entity-class/src/getPropsForEntityClass.js
+++ public/scripts/get-props-for-entity-class/src/getPropsForEntityClass.js
@@ -24,7 +24,9 @@ const requireMatcher2 = /const (\w+) = require\((.*)\);/;
 const linesToDelete = [
   '/* Linked Classes */',
   '/* Serialization utilities dependencies */',
+  '/* Serialization utilities */',
   '/* Class definition */',
+  `this.parent = props.parent;`,
 
 ];
 const classMatcher = /export class (\w+) extends Item \{/;
@@ -37,17 +39,24 @@ async function handleFile(fullPath) {
   /** @type {string[]} */
   const lines = contents.split('\n')
     .map((line) => {
-      if (linesToDelete.includes(line)) {
+      if (linesToDelete.includes(line.trim())) {
         return '';
       }
       return line;
     })
+    .map((line) => line.replace(
+      `const { getAllChildClasses, serializeThis, parseThis } = require('@bimo/core-utils-serialization');`,
+      `const { getAllChildClasses } = require('@bimo/core-utils-serialization');`))
+    .map((line) => line.replace('getAndValidatePropFromProps', 'gavpfp'))
     .map((line) => line.replace(requireMatcher, 'import $1 from $2'))
     .map((line) => line.replace(requireMatcher2, 'import $1 from $2'))
     .map((line) => line.replace(/^class /, 'export class '))
     .map((line) => line.replace('module.exports = ', 'export default '))
     .map((line) => line.replace(classMatcher, 'export class $1 extends Item<$1> {'))
+    .map((line) => line.replace(`${classKey}.prototype.serializeModel = serializeThis;`, ''))
+    .map((line) => line.replace(`${classKey}.parseModel = parseThis;`, ''))
     .map((line) => line.replace(constructorPropsMatcher, `constructor(props:${classKey}Props)`));
+
   const startOfConstructor = lines.findIndex((line) => line.match(/constructor/));
   if (startOfConstructor === -1) return;
   const linesAfterConstructor = lines.slice(startOfConstructor + 1);
@@ -58,7 +67,7 @@ async function handleFile(fullPath) {
     if (!match) return false;
     return match[1];
   }).filter(Boolean);
-  const propsDeclarationLines = propNames.map((propName) => `  ${propName}: string;`);
+  const propsDeclarationLines = propNames.map((propName) => `  ${propName}?: string;`);
   lines.splice(startOfConstructor, 0, ...propsDeclarationLines);
 
   const interfaceDeclarationLines = [
@@ -73,5 +82,5 @@ async function handleFile(fullPath) {
     ...interfaceDeclarationLines,
   );
 
-  console.log(lines);
+  await fsBimo.writeFile(fullPath, lines.join('\n'), { encoding: 'utf8' });
 }
