dddd16b381522f61a70c88361bb885ef766ab0b0
Gaël Haméon <gael.hameon@gmail.com>
2023-04-22 02:16:35 +0200

    feat(public): improve script

--- public/scripts/get-props-for-entity-class/src/getPropsForEntityClass.js
+++ public/scripts/get-props-for-entity-class/src/getPropsForEntityClass.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 const { fsBimo, getAllFilesRecursively } = require('@bimo/core-utils-filesystem');
+const { partition } = require('lodash');
 const path = require('path');
 
 async function execute(rootDir) {
@@ -8,17 +9,18 @@ async function execute(rootDir) {
   const allFiles = [];
   await getAllFilesRecursively(rootDir, allFiles);
 
-  const entityFiles = allFiles.filter((file) => !file.name.match(/Collection\.ts$/));
+  const [entityFiles, collectionFiles] = partition(allFiles, (file) => !file.name.match(/Collection\.ts$/));
   const [firstFile] = entityFiles;
   const myFiles = [firstFile];
-  await Promise.all(entityFiles.map(async (file) => handleFile(file.fullPath)));
+  await Promise.all(entityFiles.map(async (file) => handleFile(file.fullPath, { collection: false })));
+  await Promise.all(collectionFiles.map(async (file) => handleFile(file.fullPath, { collection: true })));
 
   console.log('done');
 }
 module.exports = execute;
 
 const propNameMatcher = /this\.(\w+) = /;
-const classKeyMatcher = /(\w+)\.ts/;
+const fileNameMatcher = /(\w+)\.ts/;
 const requireMatcher = /const (\{ .* \}) = require\((.*)\);/;
 const requireMatcher2 = /const (\w+) = require\((.*)\);/;
 const linesToDelete = [
@@ -29,12 +31,17 @@ const linesToDelete = [
   `this.parent = props.parent;`,
 
 ];
-const classMatcher = /export class (\w+) extends Item \{/;
-const constructorPropsMatcher = /constructor\(props\)/;
+const itemClassMatcher = /export class (\w+) extends Item \{/;
+const collectionClassMatcher = /export class (\w+) extends Collection \{/;
+const constructorPropsMatcher = /constructor\(props( = {})?\)/;
+const otherEntityMatcher = /import (\w+) from '\.\/\w+';/;
 
-async function handleFile(fullPath) {
+async function handleFile(fullPath, { collection }) {
   const contents = await fsBimo.readFile(fullPath, { encoding: 'utf8' });
-  const classKey = path.basename(fullPath).match(classKeyMatcher)[1];
+  // @ts-ignore
+  const fileName = path.basename(fullPath).match(fileNameMatcher)[1];
+  const classKey = collection ? fileName.replace('sCollection', '') : fileName;
+  const entityName = fileName;
   console.log(classKey);
   /** @type {string[]} */
   const lines = contents.split('\n')
@@ -48,14 +55,28 @@ async function handleFile(fullPath) {
       `const { getAllChildClasses, serializeThis, parseThis } = require('@bimo/core-utils-serialization');`,
       `const { getAllChildClasses } = require('@bimo/core-utils-serialization');`))
     .map((line) => line.replace('getAndValidatePropFromProps', 'gavpfp'))
-    .map((line) => line.replace(requireMatcher, 'import $1 from $2'))
-    .map((line) => line.replace(requireMatcher2, 'import $1 from $2'))
+    .map((line) => line.replace(requireMatcher, 'import $1 from $2;'))
+    .map((line) => line.replace(requireMatcher2, 'import $1 from $2;'))
     .map((line) => line.replace(/^class /, 'export class '))
     .map((line) => line.replace('module.exports = ', 'export default '))
-    .map((line) => line.replace(classMatcher, 'export class $1 extends Item<$1> {'))
-    .map((line) => line.replace(`${classKey}.prototype.serializeModel = serializeThis;`, ''))
-    .map((line) => line.replace(`${classKey}.parseModel = parseThis;`, ''))
-    .map((line) => line.replace(constructorPropsMatcher, `constructor(props:${classKey}Props)`));
+    .map((line) => line.replace(itemClassMatcher, 'export class $1 extends Item<$1> {'))
+    .map((line) => line.replace(collectionClassMatcher, `export class $1 extends Collection<${classKey}, ${classKey}Props> {`))
+    .map((line) => line.replace(`${entityName}.prototype.serializeModel = serializeThis;`, ''))
+    .map((line) => line.replace(`${entityName}.parseModel = parseThis;`, ''))
+    .map((line) => line.replace(/\/\*\* @extends .*/, ''))
+    .map((line) => line.replace(
+      `import { Collection } from '@bimo/core-utils-collection';`,
+      `import { Collection, ExtendedCollectionProps } from "@bimo/core-utils-collection";`,
+    ))
+    .map((line) => line.replace(
+      `import { Item } from '@bimo/core-utils-collection';`,
+      `import { Item, ExtendedItemProps } from "@bimo/core-utils-collection";`,
+    ))
+    .map((line) => line.replace(
+      otherEntityMatcher,
+      `import { $1, $1Props } from "./$1";`,
+    ))
+    .map((line) => line.replace(constructorPropsMatcher, `constructor(props: ${entityName}Props$1)`));
 
   const startOfConstructor = lines.findIndex((line) => line.match(/constructor/));
   if (startOfConstructor === -1) return;
@@ -71,7 +92,7 @@ async function handleFile(fullPath) {
   lines.splice(startOfConstructor, 0, ...propsDeclarationLines);
 
   const interfaceDeclarationLines = [
-    `export interface ${classKey}Props {`,
+    `export interface ${entityName}Props extends ${collection ? `ExtendedCollectionProps<${classKey}, ${classKey}Props>` : 'ExtendedItemProps'} {`,
     ...propsDeclarationLines,
     `}`,
     '',
