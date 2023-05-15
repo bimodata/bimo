140a7b714169904679e8e9b4a0a253e09b29fd54
Gaël Haméon <gael.hameon@gmail.com>
2023-05-15 22:00:21 +0200

    feat(public): add trnpdDate

--- public/core/entities/src/base-types/classes/TrainPathVariantDate.ts
+++ public/core/entities/src/base-types/classes/TrainPathVariantDate.ts
@@ -1,9 +1,11 @@
 import { Item, ExtendedItemProps } from "@bimo/core-utils-collection";
 export interface TrainPathVariantDateProps extends ExtendedItemProps {
+  trnpdDate?: string;
   trnpdEffectiveDate?: string;
   trnpdStatusOir?: string;
 }
 export declare class TrainPathVariantDate extends Item<TrainPathVariantDate> {
+  trnpdDate?: string;
   trnpdEffectiveDate?: string;
   trnpdStatusOir?: string;
   constructor(props: TrainPathVariantDateProps);
--- public/core/entities/src/class-factories/TrainPathVariantDate.ts
+++ public/core/entities/src/class-factories/TrainPathVariantDate.ts
@@ -9,17 +9,22 @@ import { Item, ExtendedItemProps } from "@bimo/core-utils-collection";
 const childClasses: (typeof Entity)[] = [];
 
 export interface TrainPathVariantDateProps extends ExtendedItemProps {
+  trnpdDate?: string;
   trnpdEffectiveDate?: string;
   trnpdStatusOir?: string;
 }
 
 export function TrainPathVariantDateClassFactory({}: EntityConstructorByEntityClassKey): typeof BimoTrainPathVariantDate {
   class TrainPathVariantDate extends Item<TrainPathVariantDate> {
+    trnpdDate?: string;
     trnpdEffectiveDate?: string;
     trnpdStatusOir?: string;
     constructor(props: TrainPathVariantDateProps) {
       super(props);
-
+      /**
+       * TODO: change date and effectiveDate for getters and setters, and infer one from the other
+       * and the info from the variant when one is not provided. */
+      this.trnpdDate = gavpfp("trnpdDate", props, `string`);
       this.trnpdEffectiveDate = gavpfp("trnpdEffectiveDate", props, `string`);
       this.trnpdStatusOir = gavpfp("trnpdStatusOir", props, `string`);
     }
