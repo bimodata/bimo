ba045ef6dc1526a672284af99752a85e2e48ee91
Gaël Haméon <gael.hameon@gmail.com>
2024-10-09 11:06:11 +0200

    feat(public): add mutedLevels option to stupidLogger

--- public/core/utils/logging/src/getStupidLogger.ts
+++ public/core/utils/logging/src/getStupidLogger.ts
@@ -1,28 +1,30 @@
 import { Logger, LogLevel } from "@bimo/core-global-types";
 
-export function getStupidLogger(silentMode = false): Logger {
-  const doNothing = function doNothing() {};
+export function getStupidLogger(
+  silentMode = false,
+  { mutedLevels }: { mutedLevels: LogLevel[] } = { mutedLevels: [] }
+): Logger {
 
-  const functionToUse = silentMode
-    ? doNothing
-    : (message: string, level: LogLevel) => {
+  const logMessage = (message: string, level: LogLevel) => {
         // eslint-disable-next-line no-console
+        if (silentMode || mutedLevels.includes(level)) return
         console.log(`logger(${level}): ${message}`);
       };
+      
   const logOrThrow = (level: LogLevel | "throw", message: string) => {
     if (level === "throw") throw new Error(message);
-    return functionToUse(message, level);
+    return logMessage(message, level);
   };
   return {
     OFF: () => {},
-    silly: (message: string) => functionToUse(message, `silly`),
-    trace: (message: string) => functionToUse(message, `trace`),
-    debug: (message: string) => functionToUse(message, `debug`),
-    info: (message: string) => functionToUse(message, `info`),
-    progress: (message: string) => functionToUse(message, `progress`),
-    warning: (message: string) => functionToUse(message, `warn`),
-    warn: (message: string) => functionToUse(message, `warn`),
-    error: (message: string) => functionToUse(message, `error`),
+    silly: (message: string) => logMessage(message, `silly`),
+    trace: (message: string) => logMessage(message, `trace`),
+    debug: (message: string) => logMessage(message, `debug`),
+    info: (message: string) => logMessage(message, `info`),
+    progress: (message: string) => logMessage(message, `progress`),
+    warning: (message: string) => logMessage(message, `warn`),
+    warn: (message: string) => logMessage(message, `warn`),
+    error: (message: string) => logMessage(message, `error`),
     throw: (message: string) => {
       throw new Error(message);
     },
